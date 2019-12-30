e212a9bbf7816a94fe546f74e805550367b78fbb
Salvatore <salvatore@arscenic.org>
2019-12-30 10:56:50 +0100

    [Salvatore] [source:trunk/lang/ geol] Export depuis http://trad.spip.net

--- lang/geol_fr.php
+++ lang/geol_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite/trunk/geodiversite/lang/
+// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite/trunk/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
