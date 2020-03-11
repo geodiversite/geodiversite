00f24393a4cd24c0a50434e6f0c5a4f25c9e5654
Salvatore <salvatore@rezo.net>
2020-03-11 01:27:15 +0100

    [Salvatore] [source:lang/ urls] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ urls] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ urls] Mise a jour du bilan depuis https://trad.spip.net

--- lang/urls_es.php
+++ lang/urls_es.php
@@ -1,9 +1,14 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/urls-geodiversite?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'titre_type_geodiv' => 'URLs de Geodiversidad'
-
+	// T
+	'titre_type_geodiv' => 'URLs de Geodiversidad'
 );
--- lang/urls_fr.php
+++ lang/urls_fr.php
@@ -1,9 +1,12 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'titre_type_geodiv' => 'URLs de Géodiversité'
-
+	// T
+	'titre_type_geodiv' => 'URLs de Géodiversité'
 );
