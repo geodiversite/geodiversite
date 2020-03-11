a557c75c2f75c83b316481dbf8c20cf4e83daffa
Salvatore <salvatore@rezo.net>
2020-03-11 01:27:11 +0100

    [Salvatore] [source:lang/ spip] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ spip] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ spip] Mise a jour du bilan depuis https://trad.spip.net

--- lang/spip_es.php
+++ lang/spip_es.php
@@ -1,12 +1,17 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/spip-geodiversite?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'forum_message_definitif' => 'Confirmar el envío',
-'form_forum_identifiant_mail' => 'Su nuevo identificador acaba de ser enviado por correo electrónico (consulte por si acaso su bandeja de correo no deseado).',
-'form_pet_message_commentaire' => 'Añadir su comentario aquí',
-'forum_voir_avant' => 'Previsualizar'
-
+	// F
+	'form_forum_identifiant_mail' => 'Su nuevo identificador acaba de ser enviado por correo electrónico (consulte por si acaso su bandeja de correo no deseado).',
+	'form_pet_message_commentaire' => 'Añadir su comentario aquí',
+	'forum_message_definitif' => 'Confirmar el envío',
+	'forum_voir_avant' => 'Previsualizar'
 );
--- lang/spip_fr.php
+++ lang/spip_fr.php
@@ -1,12 +1,15 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://github.com/geodiversite/geodiversite.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'forum_message_definitif' => 'Confirmer l\'envoi',
-'form_forum_identifiant_mail' => 'Votre nouvel identifiant vient de vous être envoyé par email (pensez à consulter votre dossier de courriers indésirables).',
-'form_pet_message_commentaire' => 'Ajoutez votre commentaire ici',
-'forum_voir_avant' => 'Prévisualiser'
-
+	// F
+	'form_forum_identifiant_mail' => 'Votre nouvel identifiant vient de vous être envoyé par email (pensez à consulter votre dossier de courriers indésirables).',
+	'form_pet_message_commentaire' => 'Ajoutez votre commentaire ici',
+	'forum_message_definitif' => 'Confirmer l’envoi',
+	'forum_voir_avant' => 'Prévisualiser'
 );
