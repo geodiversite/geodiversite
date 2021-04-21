ca5d44b42f45f7b59f869d84aa4794f72ba7c4ca
brunob <bruno@eliaz.fr>
2021-04-21 16:02:38 +0200

    vieux js inutile

--- javascript/geol.js
+++ javascript/geol.js
@@ -1,19 +1,6 @@
 (function($){
 	$(function(){
-		
-		// Faux placeholder pour le formulaire de recherche
-		$('#formulaire_recherche label').css({'position':'absolute','left':'5px','top':'0'});
-		if ($('input#recherche').length > 0 && $('input#recherche').val().length == 0) {
-			$('input#recherche').focusin(function(){
-				$('#formulaire_recherche label').fadeOut(1000);
-			});
-			$('input#recherche').focusout(function(){
-				$('#formulaire_recherche label').fadeIn(1000);
-			});
-		} else {
-			$('#formulaire_recherche label').hide();
-		}
-		
+
 		// Bloc dépliable pour code_embed, pompé sur mediaspip http://zone.spip.org/trac/spip-zone/browser/_squelettes_/mediaspip/mediaspip_core/trunk/javascript/mediaspip_base.js#L9
 		$('.code_embed').each(function(){
 			var parent = $(this).parents('div.embed');
