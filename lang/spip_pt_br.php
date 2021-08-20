c68b875bdff1738e5deb2b33d4adf926ddb8ad93
Ricardo Porto <rporto@calepino.com>
2021-08-21 01:27:11 +0200

    [Salvatore] [source:lang/ spip] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ spip] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/spip_pt_br.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/spip-geodiversite?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'form_forum_identifiant_mail' => 'A sua nova identificação acabou de ser enviada por e-mail (verifique se eventualmente está na pasta de spam)',
+	'form_pet_message_commentaire' => 'Inclua seu comentário aqui',
+	'forum_message_definitif' => 'Confirmar o envio',
+	'forum_voir_avant' => 'Visualizar'
+);
