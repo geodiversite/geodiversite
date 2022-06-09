b4a09c944126ea1a3ab943478d4ec7ea433a4be9
brunob <bruno@eliaz.fr>
2022-06-09 15:18:09 +0200

    form editer_media : base du mutli étapes + saisie bigup
    - récupérer automatiquement l'éventuel article en cours de rédaction dans le secteur des médias => plus besoin de passer de paramètre au formulaire lors de son appel (on ne souhaite pas l'utiliser pour modifier des médias déjà publiés)
    - fix sur l'affichage de la licence de l'article en cours d'édition (à vérifier que la licence par défaut appliquée dans la conf est bien prise en compte)

--- formulaires/editer_media.php
+++ formulaires/editer_media.php
@@ -6,25 +6,26 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 include_spip('inc/autoriser');
 
-function formulaires_editer_media_charger($id_article='new'){
+function formulaires_editer_media_charger(){
 
-	$valeurs = array();
+	$valeurs = array(
+		'editable' => true,
+		'_bigup_rechercher_fichiers' => true,
+		'_etapes' => 2
+	);
 	$id_rubrique = lire_config('geol/secteur_medias', 1);
 
+	// récupérer l'éventuel article de l'auteur en cours de rédaction dans le secteur des médias
+	$id_article = sql_getfetsel('id_article', "spip_articles AS article LEFT JOIN spip_auteurs_liens AS lien ON article.id_article=lien.id_objet AND lien.objet='article'", "article.statut='prepa' AND lien.id_auteur=" . intval($GLOBALS['visiteur_session']['id_auteur']) . " AND article.id_secteur=$id_rubrique");
+
 	if (!intval($id_article) and (autoriser('publierdans', 'rubrique', $id_rubrique) or autoriser('configurer'))) {
-		$valeurs = array(
-			'titre' => '',
-			'texte' => '',
-			'editable' => true
-		);
+		$valeurs['editable'] = true;
 	} elseif (autoriser('modifier', 'article', $id_article)) {
 		$article = sql_fetsel('*','spip_articles','id_article=' . intval($id_article));
-		$valeurs = array(
-			'id_article' => $id_article,
-			'titre' => $article['titre'],
-			'texte' => $article['texte'],
-			'editable' => true
-		);
+		$valeurs['id_article'] = $id_article;
+		$valeurs['titre'] = $article['titre'];
+		$valeurs['texte'] = $article['texte'];
+		$valeurs['id_licence'] = $article['id_licence'];
 		$gis = sql_fetsel('*',
 			'spip_gis as gis LEFT JOIN spip_gis_liens as gis_liens ON gis.id_gis=gis_liens.id_gis',
 			'gis_liens.objet="article" AND gis_liens.id_objet='.intval($id_article)
@@ -44,7 +45,7 @@ function formulaires_editer_media_charger($id_article='new'){
 }
 
 
-function formulaires_editer_media_verifier($id_article='new'){
+function formulaires_editer_media_verifier(){
 
 	$erreurs = array();
 	
@@ -57,7 +58,7 @@ function formulaires_editer_media_verifier($id_article='new'){
 }
 
 
-function formulaires_editer_media_traiter($id_article='new'){
+function formulaires_editer_media_traiter(){
 	
 	$res = array();
 	$id_rubrique = lire_config('geol/secteur_medias', 1);
