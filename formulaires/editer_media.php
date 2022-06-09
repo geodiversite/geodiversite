dd36b8fc6d5160a7e565daf4eb12d6d0291b5a2f
brunob <bruno@eliaz.fr>
2022-06-09 19:35:00 +0200

    form editer_media : mise en place multi étape & upload du document
    - au chargement, s'il y a un article en cours de rédaction, vérifier qu'il porte déjà un document, si c'est le cas on passe à l'étape 2
    - dans la vérification de l'étape 1, créer l'article si nécessaire et lui attacher le fichier en tant que document
    - suffixer les fonctions du form avec un _dist

--- formulaires/editer_media.php
+++ formulaires/editer_media.php
@@ -6,17 +6,22 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 include_spip('inc/autoriser');
 
-function formulaires_editer_media_charger(){
+function formulaires_editer_media_charger_dist(){
 
 	$valeurs = array(
 		'editable' => true,
 		'_bigup_rechercher_fichiers' => true,
 		'_etapes' => 2
 	);
+
 	$id_rubrique = lire_config('geol/secteur_medias', 1);
 
 	// récupérer l'éventuel article de l'auteur en cours de rédaction dans le secteur des médias
-	$id_article = sql_getfetsel('id_article', "spip_articles AS article LEFT JOIN spip_auteurs_liens AS lien ON article.id_article=lien.id_objet AND lien.objet='article'", "article.statut='prepa' AND lien.id_auteur=" . intval($GLOBALS['visiteur_session']['id_auteur']) . " AND article.id_secteur=$id_rubrique");
+	$id_article = sql_getfetsel(
+		'id_article',
+		"spip_articles AS article LEFT JOIN spip_auteurs_liens AS lien ON article.id_article=lien.id_objet AND lien.objet='article'",
+		"article.statut='prepa' AND lien.id_auteur=" . intval($GLOBALS['visiteur_session']['id_auteur']) . " AND article.id_secteur=$id_rubrique"
+	);
 
 	if (!intval($id_article) and (autoriser('publierdans', 'rubrique', $id_rubrique) or autoriser('configurer'))) {
 		$valeurs['editable'] = true;
@@ -36,6 +41,15 @@ function formulaires_editer_media_charger(){
 			$valeurs['lon'] = $gis['lon'];
 			$valeurs['zoom'] = $gis['zoom'];
 		}
+		// récupérer l'éventuel document lié à l'article en cours => étape 2
+		$id_document = sql_getfetsel(
+			'document.id_document',
+			"spip_documents AS document LEFT JOIN spip_documents_liens AS lien ON lien.id_document=document.id_document AND lien.objet='article'",
+			"lien.id_objet=$id_article"
+		);
+		if ($id_document) {
+			$valeurs['_etape'] = 2;
+		}
 	} else {
 		$valeurs['editable'] = false;
 		$valeurs['message_erreur'] = _T('info_acces_interdit');
@@ -45,33 +59,52 @@ function formulaires_editer_media_charger(){
 }
 
 
-function formulaires_editer_media_verifier(){
+function formulaires_editer_media_verifier_etape_dist($etape){
 
 	$erreurs = array();
-	
-	// ça ne devrait pas arriver mais on ne sait jamais...
-	if (!_request('titre')) {
-		$erreurs['titre'] = _T('info_obligatoire');
+	$id_rubrique = lire_config('geol/secteur_medias', 1);
+	$id_article = _request('id_article');
+
+	if ($etape == 1) {
+		// fichier obligatoire
+		if (!isset($_FILES['media']) or !file_exists($_FILES['media']['tmp_name'])) {
+			$erreurs['media'] = _T('info_obligatoire');
+		} else {
+			// créer l'article s'il n'y en a pas
+			if (!intval($id_article)) {
+				$id_article = objet_inserer('article', $id_rubrique);
+				set_request('id_article', $id_article);
+				objet_instituer('article', $id_article, array('statut' => 'prepa'));
+			}
+			// lier le fichier en doc joint à l'article
+			include_spip('action/ajouter_documents');
+			$ajouter_document = charger_fonction('ajouter_un_document', 'action');
+			$id_document = $ajouter_document('new', $_FILES['media'], 'article', $id_article, 'document');
+			if (!intval($id_document)) {
+				$res['media'] = $id_document;
+			}
+		}
+	}
+	if ($etape == 2) {
+		// ça ne devrait pas arriver mais on ne sait jamais...
+		if (!_request('titre')) {
+			$erreurs['titre'] = _T('info_obligatoire');
+		}
 	}
 
 	return $erreurs;
 }
 
 
-function formulaires_editer_media_traiter(){
+function formulaires_editer_media_traiter_dist(){
 	
 	$res = array();
-	$id_rubrique = lire_config('geol/secteur_medias', 1);
+	$id_article = _request('id_article');
 	
 	include_spip('action/editer_objet');
 	include_spip('action/editer_liens');
 	
-	// créer l'article et le publier
-	if (!intval($id_article)) {
-		$id_article = objet_inserer('article', $id_rubrique);
-		objet_instituer('article', $id_article, array('statut' => 'publie'));
-	}
-	// sinon les mettre à jour
+	// mettre à jour l'article
 	objet_modifier('article', $id_article);
 
 	// créer le point ou le mettre à jour
