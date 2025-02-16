094936416cca373753aee2fa58047a8e8cedd10c
brunob <bruno@eliaz.fr>
2025-02-16 20:40:00 +0100

    fix: réparer nos vignettes de docs persos pour SPIP 4.4
    suivre medias sur https://git.spip.net/spip/medias/-/commit/76559413e10057142ee43f6928091b7e43813e82

--- geol_fonctions.php
+++ geol_fonctions.php
@@ -34,25 +34,21 @@ if (!defined('_DIR_PLUGIN_GEOL_ALBUMS')) {
  * Vignette pour une extension de document
  *
  * Recherche les fichiers d'icones au format SVG pour l'extension demandée.
- * On cherche prive/vignettes/ext.png dans le path.
- *
- * Surcharge : dans l'espace public, pour les extensions acceptées par geodiv autres que images,
- * on cherche images/vignettes/ext.png dans le path.
+ * On cherche prive/vignettes/ext.svg dans le path.
  *
  * @param string $ext
  *     Extension du fichier. Exemple : png
- * @param bool $size
- *     true pour retourner un tableau avec les tailles de la vignette
- *     false pour retourner uniquement le chemin du fichier
+ * @param string $media
+ *     Permet de retourner une variante de la vignette adaptee au media
+ *     cas des mp4 par exemple que l'on decline quand c'est un audio
  * @param bool $loop
  *     Autoriser la fonction à s'appeler sur elle-même
  *     (paramètre interne).
- * @return array|bool|string
- *     False si l'image n'est pas trouvée
+ * @return ?string
+ *     null si l'image n'est pas trouvée
  *     Chaîne (chemin vers l'image) si on ne demande pas de taille
- *     Tableau (chemin, largeur, hauteur) si on demande avec la taille.
  */
-function inc_vignette($ext, $size = true, $loop = true) {
+function inc_vignette($ext, $media = '', $loop = true) {
 
 	if (!$ext) {
 		$ext = 'txt';
@@ -61,31 +57,22 @@ function inc_vignette($ext, $size = true, $loop = true) {
 	// chercher dans les vignettes personnalisées de geodiv
 	// dans l'espace public & pour les fichiers qui proposent une vignette perso
 	if (!test_espace_prive() and in_array($ext, array_merge(_GEOL_FICHIERS_AUDIOS, _GEOL_FICHIERS_VIDEOS, _GEOL_FICHIERS_TEXTES))) {
-		$v = find_in_path('images/vignettes/' . $ext . '.png');
+		return find_in_path('images/vignettes/' . $ext . '.png');
 	}
 	// Chercher la vignette correspondant a ce type de document
 	// dans les vignettes persos, ou dans les vignettes standard
-	if (
-		# installation dans un dossier /vignettes personnel, par exemple /squelettes/vignettes
-		!isset($v) and !$v = find_in_path('prive/vignettes/' . $ext . '.svg')
-	) {
+	# installation dans un dossier /vignettes personnel, par exemple /squelettes/vignettes
+	if (!$media || !($v = find_in_path('prive/vignettes/' . $ext . '-' . $media . '.svg'))) {
+		$v = find_in_path('prive/vignettes/' . $ext . '.svg');
+	}
+	if (!$v) {
 		if ($loop) {
 			$f = charger_fonction('vignette', 'inc');
-			$v = $f('defaut', false, $loop = false);
+			$v = $f('defaut', $media, false);
 		} else {
 			$v = false;
 		}
 	} # pas trouve l'icone de base
 
-	if (!$size) {
-		return $v;
-	}
-
-	$largeur = $hauteur = 0;
-	if ($v and $size = @spip_getimagesize($v)) {
-		$largeur = $size[0];
-		$hauteur = $size[1];
-	}
-
-	return [$v, $largeur, $hauteur];
+	return $v ?: null;
 }
