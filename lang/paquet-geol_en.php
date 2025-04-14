974b51078dfb9114147886ac1a7f729ab7696f53
BaptisteG <baptiste.grange@etu.univ-amu.fr>
2025-04-15 01:30:03 +0200

    i18n: [Salvatore] [source:lang/ paquet-geol] Export depuis https://trad.spip.net de la langue en i18n: [Salvatore] [source:lang/ paquet-geol] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-geol_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-geol?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'geol_description' => 'Skeleton of the Géodiversité.net. project',
+	'geol_slogan' => 'Skeleton of the Géodiversité project'
+);
